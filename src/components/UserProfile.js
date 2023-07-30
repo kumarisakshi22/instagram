f237edcf5596cbd167fc82017ace87db721cd336
Kumari Sakshi <98682833+kumarisakshi22@users.noreply.github.com>
2023-07-30 18:52:26 +0530

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -18,7 +18,7 @@ export default function UserProfile() {
 
 // to follow user
   const followUser = (userId) => {
-    fetch("http://localhost:5000/follow", {
+    fetch("/follow", {
       method: "put",
       headers: {
         "Content-Type": "application/json",
@@ -38,7 +38,7 @@ export default function UserProfile() {
 
      // to unfollow user
   const unfollowUser = (userId) => {
-    fetch("http://localhost:5000/unfollow", {
+    fetch("/unfollow", {
       method: "put",
       headers: {
         "Content-Type": "application/json",
@@ -59,7 +59,7 @@ export default function UserProfile() {
 
     
   useEffect(() => {
-    fetch(`http://localhost:5000/user/${userid}`, {
+    fetch(`/user/${userid}`, {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("jwt"),
       },
